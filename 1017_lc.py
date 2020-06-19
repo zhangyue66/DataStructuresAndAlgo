b02c92e1bd670ed816019a20899bca2fb684b7e5
Yue Zhang <58124290+zhangyue66@users.noreply.github.com>
2020-06-19 01:52:59 -0700

    Create 1017_lc.py

---
+++ 1017_lc.py
@@ -0,0 +1,10 @@
+class Solution:
+    def baseNeg2(self, N):
+        if N == 0:
+            return "0"
+        else:
+            res = []
+            while N:
+                res.append(N&1)
+                N = -(N>>1)
+            return "".join(str(e) for e in res[::-1])
