32ee640f10d7147f37eb617ceda843e4fe03a086
Yue Zhang <58124290+zhangyue66@users.noreply.github.com>
2020-06-19 01:54:31 -0700

    Update 1017_lc.py

--- 1017_lc.py
+++ 1017_lc.py
@@ -8,3 +8,9 @@ class Solution:
                 res.append(N&1)
                 N = -(N>>1)
             return "".join(str(e) for e in res[::-1])
+
+
+
+yz = Solution()
+N = 4
+print(yz.baseNeg2(N))
