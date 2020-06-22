f9b7307728c890d6a37ee035cc3b7e32d6681b81
Yue Zhang <58124290+zhangyue66@users.noreply.github.com>
2020-06-21 23:15:11 -0700

    Create 825_lc_binaryS.py

---
+++ 825_lc_binaryS.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numFriendRequests(self, ages):
+        if not ages: return 0
+        cnt = 0
+        N = len(ages)
+        ages.sort()
+        for i in range(N):
+            a = ages[i]
+            if a<=14: continue
+            idx1 = bisect.bisect(ages, a)
+            x = 0.5*a+7
+            idx2 = bisect.bisect(ages, x)
+            while idx2<N and ages[idx2]==x:
+                idx2 += 1
+            cnt += max(0, idx1-idx2+(-1 if idx2<=i<=idx1 else 0)) 
+        return cnt
