e7242f13533789cfb905f81cce855c6b8fa38b32
Yue Zhang <58124290+zhangyue66@users.noreply.github.com>
2020-08-11 21:12:04 -0700

    Create 287_lc_binarySearch.py

---
+++ 287_lc_binarySearch.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        low = 1
+        high = len(nums)-1
+        
+        while low < high:
+            mid = (low+high)//2
+            
+            cnt = 0
+            
+            for num in nums:
+                if num <= mid:
+                    cnt += 1
+                    
+            if cnt <= mid:
+                low = mid+1
+            else:
+                high = mid
+                
+                
+        return high
