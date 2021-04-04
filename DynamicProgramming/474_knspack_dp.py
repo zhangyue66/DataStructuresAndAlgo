97e654ca4b685b8d3e08e574c7e08ea2ac3095c4
Yue Zhang <58124290+zhangyue66@users.noreply.github.com>
2021-04-03 22:59:07 -0700

    Create 474_knspack_dp.py

---
+++ DynamicProgramming/474_knspack_dp.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMaxForm(self, strs, m, n):
+        dp = [[0] * (n + 1) for _ in range (m + 1)]
+        
+        for s in strs:
+            m1, n1 = s.count('0'), s.count('1')
+            for i in range(m, m1 - 1, -1):
+                for j in range(n, n1 - 1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i - m1][j - n1] + 1)
+        
+        return dp[m][n]
