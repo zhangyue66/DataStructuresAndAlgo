3cafd248b7a7cdb1ff81b6a69e059ba1626452ba
Yue Zhang <58124290+zhangyue66@users.noreply.github.com>
2021-05-30 18:57:42 -0700

    Create 364_bfs.py

---
+++ Searching/364_bfs.py
@@ -0,0 +1,67 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        ans = 0
+        level_sum = 0
+        queue = nestedList
+        # for element in nestedList:
+        #     queue.append(element)
+        
+        while queue:
+            next_level = []
+            for element in queue:
+                if element.isInteger():
+                    level_sum += element.getInteger()
+                else:
+                    for temp in element.getList():
+                        next_level.append(temp)
+
+            ans += level_sum
+            queue = next_level
+            
+        return ans
+            
+            
+        
