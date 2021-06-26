6a7e4771657ad93c78d7089f5710e77ff7a7c607
Yue Zhang <58124290+zhangyue66@users.noreply.github.com>
2021-06-26 16:40:55 -0700

    Create bin_search_template.py

---
+++ BinarySearch/bin_search_template.py
@@ -0,0 +1,38 @@
+# [l,r)
+
+# search value
+def bin_sec_l(nums,target):
+    # [l,r)
+    l,r = 0,len(nums)-1
+    while l < r:
+        mid = l + (r-l)//2
+        if nums[mid] == target:
+            return mid
+        if nums[mid] > target:
+            r = mid
+        else:
+            l = mid + 1
+    return -1 # not found -1 or l
+  
+  
+# lower_bound - first index of i such that nums[i] >= target
+def lower_bound(nums,target):
+  l,r = 0,len(nums)-1
+  while l < r:
+      mid = l + (r-l)//2
+      if nums[mid] >= target:
+          r = mid
+      else:
+          l = mid + 1
+  return l
+
+# upper_bound - first index of i such that nums[i] > target
+def lower_bound(nums,target):
+  l,r = 0,len(nums)-1
+  while l < r:
+      mid = l + (r-l)//2
+      if nums[mid] > target:
+          r = mid
+      else:
+          l = mid + 1
+  return l
