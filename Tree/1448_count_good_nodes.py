143524e6266380e68890e6a5a15093d6814ef102
Yue Zhang <58124290+zhangyue66@users.noreply.github.com>
2021-08-17 23:48:26 -0700

    Create 1448_count_good_nodes.py

---
+++ Tree/1448_count_good_nodes.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.ans = 0
+        def dfs(node,prev):
+            if not node:
+                return
+            if node.val >= prev:
+                self.ans += 1
+                prev = node.val
+                
+            dfs(node.left,prev)
+            dfs(node.right,prev)
+            
+        dfs(root,-float("inf"))
+        return self.ans
+            
