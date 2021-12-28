95084587e69143d8e5d49b528d150ec9c15cb424
Yue Zhang <58124290+zhangyue66@users.noreply.github.com>
2021-12-28 12:18:10 -0800

    Create 2095_fast_slow_template.py

---
+++ TwoPointer/2095_fast_slow_template.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return
+        fast,slow = head,head
+        slowPrev = None
+        
+        while fast and fast.next:
+            slowPrev = slow
+            slow = slow.next
+            fast = fast.next.next
+            
+        slowPrev.next = slow.next
+        
+        return head
